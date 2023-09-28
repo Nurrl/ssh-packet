1926b3a4e1ac2d4e319a826fa6c9dd4784eb26ce
Maya the bee <15341887+Nurrl@users.noreply.github.com>
2023-09-28 17:32:47 +0200

    Add authentication special response messages in the 'auth' module

--- src/auth.rs
+++ src/auth.rs
@@ -95,6 +95,85 @@ impl AuthMethod {
     }
 }
 
+/// The `SSH_MSG_USERAUTH_PK_OK` message.
+///
+/// see <https://datatracker.ietf.org/doc/html/rfc4252#section-7>.
+#[binrw]
+#[derive(Debug)]
+#[brw(big, magic = 60_u8)]
+pub struct AuthPkOk {
+    /// SSH_MSG_USERAUTH_PK_OK's _public key algorithm name from the request_.
+    pub algorithm: arch::String,
+
+    /// SSH_MSG_USERAUTH_PK_OK's _public key blob from the request_.
+    pub blob: arch::String,
+}
+
+/// The `SSH_MSG_USERAUTH_PASSWD_CHANGEREQ` message.
+///
+/// see <https://datatracker.ietf.org/doc/html/rfc4252#section-8>.
+#[binrw]
+#[derive(Debug)]
+#[brw(big, magic = 60_u8)]
+pub struct AuthPasswdChangereq {
+    /// SSH_MSG_USERAUTH_PASSWD_CHANGEREQ's _prompt_.
+    pub prompt: arch::StringUtf8,
+
+    /// SSH_MSG_USERAUTH_PASSWD_CHANGEREQ's _language tag_ (deprecated).
+    pub language: arch::StringAscii,
+}
+
+/// The `SSH_MSG_USERAUTH_INFO_REQUEST` message.
+///
+/// see <https://datatracker.ietf.org/doc/html/rfc4256#section-3.2>.
+#[binrw]
+#[derive(Debug)]
+#[brw(big, magic = 60_u8)]
+pub struct AuthInfoRequest {
+    /// SSH_MSG_USERAUTH_INFO_REQUEST's _name_.
+    pub name: arch::StringUtf8,
+
+    /// SSH_MSG_USERAUTH_INFO_REQUEST's _instruction_.
+    pub instruction: arch::StringUtf8,
+
+    /// SSH_MSG_USERAUTH_INFO_REQUEST's _language tag_ (deprecated).
+    pub language: arch::StringAscii,
+
+    #[bw(calc = prompts.len() as u32)]
+    num_prompts: u32,
+
+    /// SSH_MSG_USERAUTH_INFO_REQUEST's _prompts_.
+    #[br(count = num_prompts)]
+    pub prompts: Vec<AuthInfoRequestPrompt>,
+}
+
+/// A prompt in the `SSH_MSG_USERAUTH_INFO_REQUEST` message.
+#[binrw]
+#[derive(Debug)]
+#[brw(big)]
+pub struct AuthInfoRequestPrompt {
+    /// SSH_MSG_USERAUTH_INFO_REQUEST's _prompt text_.
+    pub prompt: arch::StringUtf8,
+
+    /// SSH_MSG_USERAUTH_INFO_REQUEST's _echo_.
+    pub echo: arch::Bool,
+}
+
+/// The `SSH_MSG_USERAUTH_INFO_RESPONSE` message.
+///
+/// see <https://datatracker.ietf.org/doc/html/rfc4256#section-3.4>.
+#[binrw]
+#[derive(Debug)]
+#[brw(big, magic = 61_u8)]
+pub struct AuthInfoResponse {
+    #[bw(calc = responses.len() as u32)]
+    num_responses: u32,
+
+    /// SSH_MSG_USERAUTH_INFO_RESPONSE's _responses_.
+    #[br(count = num_responses)]
+    pub responses: Vec<arch::StringUtf8>,
+}
+
 /// The `SSH_MSG_USERAUTH_FAILURE` message.
 ///
 /// see <https://datatracker.ietf.org/doc/html/rfc4252#section-5.1>.
