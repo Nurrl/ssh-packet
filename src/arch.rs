34c30e20fcf975243d7e08c4b75e89fde744b542
Maya the bee <15341887+Nurrl@users.noreply.github.com>
2023-09-28 16:50:35 +0200

    Improved String* and Bool by implementing some traits

--- src/arch.rs
+++ src/arch.rs
@@ -1,7 +1,7 @@
 //! Types defined in the SSH's **architecture** part of the protocol,
 //! as defined in the [RFC 4251](https://datatracker.ietf.org/doc/html/rfc4251).
 
-use std::string::String as StdString;
+use std::{borrow::Cow, string::String as StdString};
 
 use binrw::binrw;
 
@@ -20,6 +20,14 @@ pub struct String {
     payload: Vec<u8>,
 }
 
+impl std::ops::Deref for String {
+    type Target = [u8];
+
+    fn deref(&self) -> &Self::Target {
+        self.payload.as_ref()
+    }
+}
+
 /// A `string` as defined in the SSH protocol,
 /// prefixed with it's `size` as a [`u32`],
 /// restricted to valid **UTF-8**.
@@ -32,9 +40,23 @@ pub struct StringUtf8 {
     #[bw(calc = payload.len() as u32)]
     size: u32,
 
-    #[br(try_map = StdString::from_utf8, count = size)]
-    #[bw(map = StdString::as_bytes)]
-    payload: StdString,
+    #[br(try_map = |bytes: Vec<u8>| StdString::from_utf8(bytes).map(Cow::Owned), count = size)]
+    #[bw(map = |payload| payload.as_bytes())]
+    payload: Cow<'static, str>,
+}
+
+impl StringUtf8 {
+    pub fn new(s: impl Into<Cow<'static, str>>) -> Self {
+        Self { payload: s.into() }
+    }
+}
+
+impl std::ops::Deref for StringUtf8 {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        self.payload.as_ref()
+    }
 }
 
 /// A `string` as defined in the SSH protocol,
@@ -49,10 +71,24 @@ pub struct StringAscii {
     #[bw(calc = payload.len() as u32)]
     size: u32,
 
-    #[br(try_map = StdString::from_utf8, count = size)]
-    #[bw(map = StdString::as_bytes)]
+    #[br(try_map = |bytes: Vec<u8>| StdString::from_utf8(bytes).map(Cow::Owned), count = size)]
+    #[bw(map = |payload| payload.as_bytes())]
     #[brw(assert(payload.chars().all(|ch| ch.is_ascii())))]
-    payload: StdString,
+    payload: Cow<'static, str>,
+}
+
+impl StringAscii {
+    pub fn new(s: impl Into<Cow<'static, str>>) -> Self {
+        Self { payload: s.into() }
+    }
+}
+
+impl std::ops::Deref for StringAscii {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        self.payload.as_ref()
+    }
 }
 
 /// A `name-list` as defined in the SSH protocol,
@@ -74,5 +110,21 @@ pub struct NameList(StringAscii);
 pub struct Bool(
     #[br(map = |n: u8| n > 0)]
     #[bw(map = |b| u8::from(*b))]
-    bool,
+    pub bool,
 );
+
+impl std::ops::Not for Bool {
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        Self(!self.0)
+    }
+}
+
+impl std::ops::Deref for Bool {
+    type Target = bool;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
